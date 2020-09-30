5466475c4224a06bbc1471b5bf6b13b045c1b7b1
legenddiv007 <61193137+legenddiv007@users.noreply.github.com>
2020-10-01 01:01:58 +0530

    Update codeforces 1367B.cpp

--- codeforces 1367B.cpp
+++ codeforces 1367B.cpp
@@ -3,13 +3,17 @@ using namespace std;
 int main()
 {
     int t;
+    cout<<"Enter value of t";
     cin>>t;
     while(t--)
     {
         int n,even=0,odd=0,fine=0;
+        cout<<"Enter value of n";
         cin>>n;
         int arr[n+1];
-        for(int i=0;i<n;i++)  cin>>arr[i];
+        cout<<"Enter array elements";
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
         for(int i=0;i<n;i++)
         {
             if(i%2==0 && arr[i]%2!=0) even++;
@@ -22,3 +26,4 @@ int main()
 
     }
 }
+
